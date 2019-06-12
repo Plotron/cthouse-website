992d1a406b01c9dc4fe8ab00f5339f79038eb55c
AdiBier <AdiBier@interia.eu>
2019-06-12 02:16:54 +0200

    Add: model first view pom

--- src/main/java/pl/cthouse/www/demo/controller/HomeController.java
+++ src/main/java/pl/cthouse/www/demo/controller/HomeController.java
@@ -1,7 +1,11 @@
 package pl.cthouse.www.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-class HomeController {
+    public class HomeController {
+
+    @RequestMapping(path = "/")
+    public String home() { return "home";}
 }
